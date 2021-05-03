f2c4798f4d0fcf67ade76acbeb144e2d0b6336f4
Robin Andersson <roban591@student.liu.se>
2021-05-03 13:15:41 +0200

    Implement support for classes and remove AstPrinter.

--- src/com/craftinginterpreters/lox/Stmt.java
+++ src/com/craftinginterpreters/lox/Stmt.java
@@ -3,30 +3,21 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 abstract class Stmt {
-  abstract <R> R accept(Visitor<R> visitor);
-
   interface Visitor<R> {
     R visitBlockStmt(Block stmt);
-
+    R visitClassStmt(Class stmt);
     R visitExpressionStmt(Expression stmt);
-
     R visitFunctionStmt(Function stmt);
-
     R visitIfStmt(If stmt);
-
     R visitPrintStmt(Print stmt);
-
     R visitReturnStmt(Return stmt);
-
     R visitVarStmt(Var stmt);
-
     R visitWhileStmt(While stmt);
   }
-
   static class Block extends Stmt {
     final List<Stmt> statements;
 
-    Block(List<Stmt> statements) {
+    Block(List<Stmt> statements){
       this.statements = statements;
     }
 
@@ -35,11 +26,24 @@ abstract class Stmt {
       return visitor.visitBlockStmt(this);
     }
   }
+  static class Class extends Stmt {
+    final Token name;
+    final List<Stmt.Function> methods;
 
+    Class(Token name, List<Stmt.Function> methods){
+      this.name = name;
+      this.methods = methods;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitClassStmt(this);
+    }
+  }
   static class Expression extends Stmt {
     final Expr expression;
 
-    Expression(Expr expression) {
+    Expression(Expr expression){
       this.expression = expression;
     }
 
@@ -48,13 +52,12 @@ abstract class Stmt {
       return visitor.visitExpressionStmt(this);
     }
   }
-
   static class Function extends Stmt {
     final Token name;
     final List<Token> params;
     final List<Stmt> body;
 
-    Function(Token name, List<Token> params, List<Stmt> body) {
+    Function(Token name, List<Token> params, List<Stmt> body){
       this.name = name;
       this.params = params;
       this.body = body;
@@ -65,13 +68,12 @@ abstract class Stmt {
       return visitor.visitFunctionStmt(this);
     }
   }
-
   static class If extends Stmt {
     final Expr condition;
     final Stmt thenBranch;
     final Stmt elseBranch;
 
-    If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+    If(Expr condition, Stmt thenBranch, Stmt elseBranch){
       this.condition = condition;
       this.thenBranch = thenBranch;
       this.elseBranch = elseBranch;
@@ -82,11 +84,10 @@ abstract class Stmt {
       return visitor.visitIfStmt(this);
     }
   }
-
   static class Print extends Stmt {
     final Expr expression;
 
-    Print(Expr expression) {
+    Print(Expr expression){
       this.expression = expression;
     }
 
@@ -95,12 +96,11 @@ abstract class Stmt {
       return visitor.visitPrintStmt(this);
     }
   }
-
   static class Return extends Stmt {
     final Token keyword;
     final Expr value;
 
-    Return(Token keyword, Expr value) {
+    Return(Token keyword, Expr value){
       this.keyword = keyword;
       this.value = value;
     }
@@ -110,12 +110,11 @@ abstract class Stmt {
       return visitor.visitReturnStmt(this);
     }
   }
-
   static class Var extends Stmt {
     final Token name;
     final Expr initializer;
 
-    Var(Token name, Expr initializer) {
+    Var(Token name, Expr initializer){
       this.name = name;
       this.initializer = initializer;
     }
@@ -125,12 +124,11 @@ abstract class Stmt {
       return visitor.visitVarStmt(this);
     }
   }
-
   static class While extends Stmt {
     final Expr condition;
     final Stmt body;
 
-    While(Expr condition, Stmt body) {
+    While(Expr condition, Stmt body){
       this.condition = condition;
       this.body = body;
     }
@@ -140,4 +138,6 @@ abstract class Stmt {
       return visitor.visitWhileStmt(this);
     }
   }
+
+  abstract <R> R accept(Visitor<R> visitor);
 }
--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -20,9 +20,12 @@ public class GenerateAst {
             "Assign   : Token name, Expr value",
             "Binary   : Expr left, Token operator, Expr right",
             "Call     : Expr callee, Token paren, List<Expr> arguments",
+            "Get      : Expr object, Token name",
             "Grouping : Expr expression",
             "Literal  : Object value",
             "Logical  : Expr left, Token operator, Expr right",
+            "Set      : Expr object, Token name, Expr value",
+            "This     : Token keyword",
             "Unary    : Token operator, Expr right",
             "Variable : Token name"));
 
@@ -31,6 +34,7 @@ public class GenerateAst {
         "Stmt",
         Arrays.asList(
             "Block      : List<Stmt> statements",
+            "Class      : Token name, List<Stmt.Function> methods",
             "Expression : Expr expression",
             "Function   : Token name, List<Token> params, List<Stmt> body",
             "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
@@ -62,19 +66,19 @@ public class GenerateAst {
 
     // The base accept() method.
     writer.println();
-    writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
     writer.println("}");
     writer.close();
   }
 
   private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
-    writer.println("    interface Visitor<R> {");
+    writer.println("  interface Visitor<R> {");
 
     for (String type : types) {
       String typeName = type.split(":")[0].trim();
       writer.println(
-          "        R visit"
+          "    R visit"
               + typeName
               + baseName
               + "("
@@ -84,38 +88,38 @@ public class GenerateAst {
               + ");");
     }
 
-    writer.println("    }");
+    writer.println("  }");
   }
 
   private static void defineType(
       PrintWriter writer, String baseName, String className, String fieldList) {
-    writer.println("    static class " + className + " extends " + baseName + " {");
+    writer.println("  static class " + className + " extends " + baseName + " {");
 
     // Fields
     String[] fields = fieldList.split(", ");
     for (String field : fields) {
-      writer.println("        final " + field + ";");
+      writer.println("    final " + field + ";");
     }
 
     // Constructor.
     writer.println();
-    writer.println("        " + className + "(" + fieldList + ")" + "{");
+    writer.println("    " + className + "(" + fieldList + ")" + "{");
 
     // Store parameters in fields.
     for (String field : fields) {
       String name = field.split(" ")[1];
-      writer.println("            this." + name + " = " + name + ";");
+      writer.println("      this." + name + " = " + name + ";");
     }
 
-    writer.println("        }");
+    writer.println("    }");
 
     // Visitor pattern.
     writer.println();
-    writer.println("        @Override");
-    writer.println("        <R> R accept(Visitor<R> visitor) {");
-    writer.println("            return visitor.visit" + className + baseName + "(this);");
-    writer.println("        }");
-
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
     writer.println("    }");
+
+    writer.println("  }");
   }
 }
